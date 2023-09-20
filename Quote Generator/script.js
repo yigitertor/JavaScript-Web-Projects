6df456e3b6bf7038ec531582730feaa9181b46a4
yigitertor <yigitertor@hotmail.com>
2023-09-21 00:29:30 +0300

    Project Update

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -11,9 +11,25 @@ let apiQuotes =[];
 
 function newQuote(){
     //Pick a random quote from ApiQuotes array
-
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length )];
-    console.log(quote);
+    
+    //Check if Author field is blank and replace it with "Unknown"
+
+    if (!quote.author){
+        authorText.textContent = "Unknown";
+    }else{
+        authorText.textContent = quote.author;
+    }
+    
+    //Check Quote lenght to determine styling
+    if(quote.text.lengt > 120){
+        quoteText.classList.add("long-quote")
+    }else{
+        quoteText.classList.remove("long-quote")
+    }
+
+    quoteText.textContent = quote.text;
+
 }
 
 // Get Quotes From API
@@ -28,6 +44,17 @@ async function getQuotes(){
         //Catch Error Here
     }
 }
+
+// Tweet Quote
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click' , tweetQuote);
 
 // On Load
 getQuotes();
