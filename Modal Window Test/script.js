8ee47835c8d9f6afdc92c436e2c20d69a561d9fd
yigitertor <61631906+yigitertor@users.noreply.github.com>
2024-02-29 20:30:03 +0300

    Update script.js

--- Modal Window Test/script.js
+++ Modal Window Test/script.js
@@ -8,4 +8,13 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', function () {
     modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
   });
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
